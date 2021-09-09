2420b6a276b53a24e018ff81325df257dc5d391b
Atahan <atahancelebi98@gmail.com>
2021-09-09 17:10:16 +0300

    webView initial commit

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://panel.cyspot.com.tr/");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
