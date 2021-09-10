ccbf2b9cfee7c6290dee1bf7ff66f583f960eb95
Atahan <atahancelebi98@gmail.com>
2021-09-10 15:11:38 +0300

    onesignal push notification

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.onesignal.OneSignal;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private static final String ONESIGNAL_APP_ID = "0dc64455-20f2-4252-bfaa-9cc8280b943f";
 
-
+//onesignal id: 0dc64455-20f2-4252-bfaa-9cc8280b943f
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://panel.cyspot.com.tr/");
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
 
     }
 
